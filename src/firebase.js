179a384cf21f10e1d5302e972e12df4dee094a0b
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-06-02 17:07:37 +0530

    basic setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfRU81-nHBIt8IKWsIFaXWdgffeKW3Nyg",
+  authDomain: "todoist-43253.firebaseapp.com",
+  projectId: "todoist-43253",
+  storageBucket: "todoist-43253.appspot.com",
+  messagingSenderId: "930990007720",
+  appId: "1:930990007720:web:b15164ce848332d273e99d",
+};
+
+export { firebaseConfig as firebase };
